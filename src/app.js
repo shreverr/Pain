5ab49429c4c84f00f0069eff7039bc8dba0dd179
shreverr <verma2007s@gmail.com>
2023-03-26 18:34:04 +0530

    static now works

--- src/app.js
+++ src/app.js
@@ -2,13 +2,18 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
+const ejs = require('ejs');
 
 const port = 5500;
 const app = express();
 
+app.use('/static', express.static(path.join(__dirname, '../assets')));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
 // Routes
 app.get("/", (req, res) => {
-  res.send("teri maa ki choot");
+  res.render('./templates/index');
 });
 
 app.listen(port, () => {
