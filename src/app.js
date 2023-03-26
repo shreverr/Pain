b1d5e0fe506c9be56cefa089f3c0c443c4bf28a1
shreverr <verma2007s@gmail.com>
2023-03-26 23:31:35 +0530

    added plslogin and favicon

--- src/app.js
+++ src/app.js
@@ -7,13 +7,17 @@ const ejs = require('ejs');
 const port = 5500;
 const app = express();
 
-app.use('/static', express.static(path.join(__dirname, '../assets')));
+app.use('/static', express.static( 'assets'));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 // Routes
 app.get("/", (req, res) => {
   res.render('./templates/index');
+});
+
+app.get("/login", (req, res) => {
+  res.render('./templates/loginpage');
 });
 
 app.listen(port, () => {
